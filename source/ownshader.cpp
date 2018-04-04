cc51c912f620100f6efc079739b594544135497d
JuliaHeise <julia.heise.95@web.de>
2018-04-04 17:08:06 +0200

    Revert "Funktionierendes Dreieck"
    This reverts commit fa6f56159a945ad6b493d1265c3cc6919e634839.

--- source/ownshader.cpp
+++ source/ownshader.cpp
@@ -4,11 +4,36 @@
 	///////////////////// 	   Work With	  /////////////////////
 	///////////////////////////////////////////////////////////////
 //-------------------------------------------------------------------------------------//
+<<<<<<< HEAD
+=======
+void Shader::draw(unsigned int VAO, unsigned int VBO) {
+        // input
+        // -----
+        processInput();
+
+        // render
+        // ------
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // draw our first triangle
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glBindVertexArray(0); // no need to unbind it every time 
+ 
+        // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+        // -------------------------------------------------------------------------------
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+}
+>>>>>>> parent of fa6f561... Funktionierendes Dreieck
 
 void Shader::setMat4(const std::string &name, const glm::mat4 &mat) const{
 	glUniformMatrix4fv(glGetUniformLocation(this->shaderProgram, name.c_str()), 1, GL_FALSE, &mat[0][0]);
 }
 //-------------------------------------------------------------------------------------//
+<<<<<<< HEAD
 Shader::Shader(GLFWwindow* window, Model<4,2>* m) : shaderProgram(0), window(window){
 if (window == NULL)
     {
@@ -163,8 +188,104 @@ Shader::~Shader() {
         // glfw: terminate, clearing all previously allocated GLFW resources.
     // ------------------------------------------------------------------
     glfwTerminate();
+=======
+
+Shader::Shader() : shaderProgram(0) {
+	/*glfwMakeContextCurrent(window);
+	//glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	///////////////////////////////////////////////////////////////
+	/////////////////////   glad func.loader  /////////////////////
+	///////////////////////////////////////////////////////////////
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+	}
+	initShader();*/
+	std::cout << "Failed: not enough Shader-Input!" << std::endl;
 }
 
+Shader::Shader(GLFWwindow* window) : shaderProgram(0), window(window){
+
+	if (window == NULL)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+	}
+	glfwMakeContextCurrent(window);
+	//glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	///////////////////////////////////////////////////////////////
+	/////////////////////   glad func.loader  /////////////////////
+	///////////////////////////////////////////////////////////////
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+	}
+}
+Shader::~Shader() {
+
+	///////////////////////////////////////////////////////////////
+	/////////////////////   shut down window  /////////////////////
+	///////////////////////////////////////////////////////////////
+	glfwTerminate();
+}
+
+void  Shader::initShader() {
+
+	///////////////////////////////////////////////////////////////
+	/////////////////////build/compile shader /////////////////////
+	///////////////////////////////////////////////////////////////
+
+	int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+	// check for shader compile errors
+	int success;
+	char infoLog[512];
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+	// fragment shader
+	int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+	// check for shader compile errors
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+	///////////////////////////////////////////////////////////////
+	/////////////////////     link  shader    /////////////////////
+	///////////////////////////////////////////////////////////////
+
+	shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+
+	// check for linking errors
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	if (!success) {
+		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+	}
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+>>>>>>> parent of fa6f561... Funktionierendes Dreieck
+}
+
+void Shader::close(unsigned int VAO, unsigned int VBO) {
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+}
 
 // process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
 // ---------------------------------------------------------------------------------------------------------
